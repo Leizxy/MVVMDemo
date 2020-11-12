ecdf250b37e82142e77e0be99ac6c6ccdee978c3
Leizxy <wl_01025@163.com>
2020-11-12 22:25:41 +0800

    test viewmodel

--- app/src/main/java/cn/leizy/mvvmdemo/MainActivity.kt
+++ app/src/main/java/cn/leizy/mvvmdemo/MainActivity.kt
@@ -1,17 +1,24 @@
 package cn.leizy.mvvmdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        viewModel.testBean
+        viewModel.apply {
+            stringData.observe(this@MainActivity, {
+                test.text = it
+                Log.i("MainActivity", "onCreate: $it")
+            })
+        }.initString()
     }
 }
--- app/src/main/java/cn/leizy/mvvmdemo/MainViewModel.kt
+++ app/src/main/java/cn/leizy/mvvmdemo/MainViewModel.kt
@@ -1,16 +1,28 @@
 package cn.leizy.mvvmdemo
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 /**
  * @author Created by wulei
  * @date 2020/11/12, 012
  * @description
  */
-class MainViewModel: ViewModel() {
-    var testBean:TestBean? = null
+class MainViewModel : ViewModel() {
+    var testBean: TestBean? = null
+
+    val stringData = MutableLiveData<String>()
+
+    fun initString() {
+        viewModelScope.launch {
+            delay(2000)
+            stringData.value = "test"
+        }
+    }
 
     override fun onCleared() {
-        testBean = null
     }
 }
